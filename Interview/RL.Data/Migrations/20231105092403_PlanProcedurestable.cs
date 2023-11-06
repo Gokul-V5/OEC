068316b6fa5a53c95f200f9df887e9beedb24af9
Gokul-V5 <149957447+Gokul-V5@users.noreply.github.com>
2023-11-06 13:51:29 +0530

    Add files via upload

---
+++ Interview/RL.Data/Migrations/20231105092403_PlanProcedurestable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RL.Data.Migrations
+{
+    public partial class PlanProcedurestable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlanProcedures",
+                table: "PlanProcedures");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "PlanProcedures",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlanProcedures",
+                table: "PlanProcedures",
+                columns: new[] { "PlanId", "ProcedureId", "UserId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlanProcedures",
+                table: "PlanProcedures");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PlanProcedures");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlanProcedures",
+                table: "PlanProcedures",
+                columns: new[] { "PlanId", "ProcedureId" });
+        }
+    }
+}
